4d89ff58923115d8177d15925e383a1b5afe323e
Sebastian Roberts <tiansebas09@gmail.com>
2023-03-13 01:42:41 -0600

    Adding fetchAPI

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,27 +1,26 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
+import { fetchAPI, submitAPI } from "./random";
 
 function BookingForm() {
+
     const currentDate = new Date()
     const [date, setDate] = useState(currentDate);
     const [time, setTime] = useState("17:00");
     const [guests, setGuests] = useState(1);
     const [occasion, setOccasion] = useState("Birthday");
-    
-    const handleDateChange = (e) => {
-        setDate(e.target.value);
-    }
+    const [initializeTimes, updateTimes] = useState(fetchAPI(currentDate))
 
-    const handleTimeChange = (e) => {
-        setTime(e.target.value);
-    }
+    useEffect(() => {
+        updateTimes(fetchAPI(new Date(date)))
+    }, [date])
 
-    const handleGuestChange = (e) => {
-        setGuests(e.target.value);
-    }
+    const handleDateChange = (e) => { setDate(e.target.value); }
 
-    const handleOccasionChange = (e) => {
-        setOccasion(e.target.value);
-    }
+    const handleTimeChange = (e) => { setTime(e.target.value); }
+
+    const handleGuestChange = (e) => { setGuests(e.target.value); }
+
+    const handleOccasionChange = (e) => { setOccasion(e.target.value); }
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -40,12 +39,9 @@ function BookingForm() {
             <input type="date" id="res-date" value={date} onChange={handleDateChange} />
             <label htmlFor="res-time">Choose time</label>
             <select id="res-time " value={time} onChange={handleTimeChange} >
-                <option>17:00</option>
-                <option>18:00</option>
-                <option>19:00</option>
-                <option>20:00</option>
-                <option>21:00</option>
-                <option>22:00</option>
+                {initializeTimes.map((time) => (
+                    <option>{time}</option>
+                ))}
             </select>
             <label htmlFor="guests">Number of guests</label>
             <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={handleGuestChange} />
