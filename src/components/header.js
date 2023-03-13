49297576c02dc47f27328da1bfd49d39750afa53
Sebastian Roberts <tiansebas09@gmail.com>
2023-03-13 15:22:19 -0600

    Adding images and svgs

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,6 @@ function Header() {
         <meta name="og:title" content="Little Lemon"/>
         <meta name="og:description" content="Little Lemon is a simple resturant"/>
         <meta name="og:image" content=""/>
-        <h1>Header Element</h1>
     </header>
     )
 }
